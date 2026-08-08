e9f3a23dba431c8b69c906cfde750a4138f5b447
spliceydice <rana.yug@yahoo.com>
2026-08-08 21:40:43 +0100

    updated player sprite/script

--- player.gd
+++ player.gd
@@ -1,25 +1,27 @@
 extends CharacterBody2D
 
+@export var speed: float = 450.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -575.0
+@export var texture_up: Texture2D
+@export var texture_down: Texture2D
+@export var texture_left: Texture2D
+@export var texture_right: Texture2D
 
+@onready var sprite = $Sprite2D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
 	move_and_slide()
+	
+	if direction != Vector2.ZERO:
+		if abs(direction.x) >= abs(direction.y):
+			if direction.x > 0 and texture_right:
+				sprite.texture = texture_right
+			elif direction.x < 0 and texture_left:
+				sprite.texture = texture_left
+		else:
+			if direction.y > 0 and texture_down:
+				sprite.texture = texture_down
+			elif direction.y < 0 and texture_up:
+				sprite.texture = texture_up
