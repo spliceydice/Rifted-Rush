30f40a266658f0b629a1adcd628413859463db50
spliceydice <rana.yug@yahoo.com>
2026-08-09 18:47:51 +0100

    updated background of finish screen

--- done_screen.gd
+++ done_screen.gd
@@ -7,3 +7,5 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://timer_screen.tscn")
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings_screen.tscn") # Replace with function body.
