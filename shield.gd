c58abc87c3ecefa94e0fcb75b55b098f76059501
spliceydice <rana.yug@yahoo.com>
2026-08-16 19:59:55 +0100

    implemented shield for minigame 3

---
+++ shield.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+# decrease rotation step for scaling later
+@export var rotation_step: float = 20.0
+
+func _unhandled_input(event) -> void:
+	if event is InputEventKey and event.pressed and not event.is_echo():
+		if rotation_degrees > -60:
+			if event.keycode == KEY_LEFT:
+				rotation_degrees -= rotation_step
+		if rotation_degrees < 60:
+			if event.keycode == KEY_RIGHT:
+				rotation_degrees += rotation_step
