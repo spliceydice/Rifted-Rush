d12e5f99aacd4aed5e51d9d57fb69af3277e9b18
spliceydice <rana.yug@yahoo.com>
2026-08-18 19:14:49 +0100

    finished minigame 3

---
+++ minigame_3.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var minigame_timer: Node2D = $minigame_timer
+var timer_end = false
+
+func _ready() -> void:
+	$RichTextLabel.activate_fade_mode()
+	await minigame_timer.Timer(6.5) # scale this
+	#after this is completed...
+	timer_end = true 
+
+func _process(delta: float) -> void:
+	if timer_end:
+		get_tree().change_scene_to_file("res://timer_screen.tscn")
+		return
+
+func _on_moon_area_entered(area: Area2D) -> void:
+	Global.lives -= 1
+	Global.minigames_done -=1
+	if Global.lives == 0:
+		get_tree().change_scene_to_file("res://death_screen.tscn")
+		return
+	else:
+		get_tree().change_scene_to_file("res://timer_screen.tscn")
+		return
