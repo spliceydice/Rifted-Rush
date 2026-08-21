f07fe21449237dae38953eba7c075431827e069d
spliceydice <rana.yug@yahoo.com>
2026-08-21 13:58:40 +0100

    updated signals that ca be given from player scene

--- player.gd
+++ player.gd
@@ -7,7 +7,16 @@ extends CharacterBody2D
 @export var texture_left: Texture2D
 @export var texture_right: Texture2D
 
+signal player_area_entered(area)
+
 @onready var sprite = $Sprite2D
+@onready var interaction_area = $Area2D
+
+func _ready():
+	interaction_area.area_entered.connect(_on_area_2d_area_entered)
+
+func _on_area_2d_area_entered(other_area):
+	player_area_entered.emit(other_area)
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
