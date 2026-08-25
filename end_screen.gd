7c39f9ad824bb72cf00e8c78997fc37edfb20017
spliceydice <rana.yug@yahoo.com>
2026-08-25 15:45:09 +0100

    reset variables

--- end_screen.gd
+++ end_screen.gd
@@ -1,6 +1,15 @@
 extends Node2D
 @onready var score_label: RichTextLabel = $Score
+@onready var high_score_label: RichTextLabel = $"High Score"
+func _ready() -> void:
+	Global.tutorial = false
+	Global.lives = 5
+	Global.minigames_done = 0
+	Global.last_minigame_picked = 0
+	if Global.score > Global.high_score:
+		Global.high_score = Global.score
 	score_label.text = "" + str(Global.score)
+	high_score_label.text = "High Score: " + str(Global.high_score)
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://timer_screen.tscn")
 	Global.score = 0
