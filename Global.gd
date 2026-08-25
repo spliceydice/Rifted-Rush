6e1e2c86963d796ae3381af21a5898cc344f93a1
spliceydice <rana.yug@yahoo.com>
2026-08-25 15:46:40 +0100

    increased difficulty earlier in the game

--- Global.gd
+++ Global.gd
@@ -4,10 +4,11 @@ var lives = 5
 var score = 0
 var tutorial = true
 var last_minigame_picked = 0
+var high_score = 0
 
 const DIFFICULTY_MAX_MINIGAME := 25
 const STEP_LEVEL := 10
-const STEP_JUMP := 0.15
+const STEP_JUMP := 0.2
 const POINTS_PER_SECOND_LEFT := 200
 const SURVIVE_COMPLETION_BONUS := 300
 const BASE_POINTS := 100
