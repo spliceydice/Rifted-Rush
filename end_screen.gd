9026f39d2924b78b5d5d3d34c0a77a4a6a7a5f4b
spliceydice <rana.yug@yahoo.com>
2026-08-25 15:44:54 +0100

    added ui buttons

--- end_screen.gd
+++ end_screen.gd
@@ -1,2 +1,11 @@
+extends Node2D
 @onready var score_label: RichTextLabel = $Score
 	score_label.text = "" + str(Global.score)
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://timer_screen.tscn")
+	Global.score = 0
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings_screen.tscn")
+	Global.score = 0
