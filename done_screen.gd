3b67d29206a22d012648cb49f9b5397af64fd7de
spliceydice <rana.yug@yahoo.com>
2026-08-29 21:34:58 +0100

    updated where quit button leads

--- done_screen.gd
+++ done_screen.gd
@@ -10,7 +10,7 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://timer_screen.tscn")
 	Global.score = 0
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://title_screen.tscn")
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://settings_screen.tscn")
 	Global.score = 0
