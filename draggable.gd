3300b242ab1e531262066bea79a50226529e4013
spliceydice <rana.yug@yahoo.com>
2026-09-19 17:53:47 +0100

    changed drag logic so mouse must be within limited radius for functionality to path

--- draggable.gd
+++ draggable.gd
@@ -3,6 +3,8 @@ extends Path2D
 @onready var path_follow: PathFollow2D = $PathFollow2D
 @onready var area_2d: Area2D = $PathFollow2D/draggable/Area2D
 
+#sets max distance for mouse where dragging occurs
+@export var max_drag_distance: float = 75.0
 var is_dragging = false
 
 func _ready() -> void:
@@ -13,10 +15,17 @@ func _process(delta: float) -> void:
 		# lets the mouses position be reused
 		var mouse_pos = get_global_mouse_position()
 		var local_mouse_pos = to_local(mouse_pos)
-		#finds the point closest on the path
-		var closest_offset = curve.get_closest_offset(local_mouse_pos)
-		#upd the position on path
-		path_follow.progress = closest_offset
+		#closest point on path calc
+		var closest_point = curve.get_closest_point(local_mouse_pos)
+		#distance between mouse and point
+		var distance_point = local_mouse_pos.distance_to(closest_point)
+		#compare distance to max allowed distance
+		if distance_point <= max_drag_distance:
+			var closest_offset = curve.get_closest_offset(local_mouse_pos)
+			path_follow.progress = closest_offset
+		else:
+			#if mouse exceeds distance parameters dragging stops
+			is_dragging = false
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	#when left click
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
