419aa2fa761898188dfb82ed16dedaaa1f6252b9
spliceydice <rana.yug@yahoo.com>
2026-09-20 15:52:55 +0100

    made text visible for longer

--- popup.gd
+++ popup.gd
@@ -1,4 +1,4 @@
 extends RichTextLabel
 func activate_fade_mode() -> void:
 		var tween = create_tween()
-		tween.tween_property(self, "modulate:a", 0.0, 0.5)
+		tween.tween_property(self, "modulate:a", 0.0, 0.75)
